5aae74189c86e5a6fdf2d03b68c10168bdeb10a5
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2025-02-16 11:41:58 -0400

    button done

--- src/components/organisms/Presentation.jsx
+++ src/components/organisms/Presentation.jsx
@@ -2,9 +2,44 @@ import './styles/Presentation.css'
 
 function Presentation() {
   return (
-    <section className="presentation">
+    <div className="presentation">
+      <img className='presentation__lines' src="src/assets/presentationLines.svg"/>
+      <span className='presentation__span'>No Pares de Estudiar</span>
+      <h3 className='presentation__phrase'>AYUDANOS A<br/> CUMPLIR TUS METAS</h3>
+      <section>
+        <div className='presentation__rectangle__long'></div>
+        <div className='presentation__rectangle__first'></div>
+        <div className='presentation__rectangle__second'></div>
+      </section>
+      <section>
+        <img className="presentation__image presentation__image__man" src="images/presentation-man.webp" alt="presentation-man" />
+        <img className="presentation__image presentation__image__calendar" src="images/presentation-calendar.webp" alt="presentation-calendar" />
+        <img className="presentation__image presentation__image__bar" src="images/presentation-bar.webp" alt="presentation-bar"/>
+      </section>
 
-    </section>
+      <section className="presentation__buttonSection">
+        <button className="presentation__buttonSection__button">
+          <svg className="presentation__buttonSection__svg" viewBox="0 0 150 150">
+            <path
+              id="circlePath"
+              d="M 75, 75 m -60, 0 a 60,60 0 1,1 120,0 a 60,60 0 1,1 -120,0"
+            />
+            <text className="presentation__buttonSection__buttonTextExplore">
+              <textPath href="#circlePath">
+                EXPLORE MORE - EXPLORE MORE -
+              </textPath>
+            </text>
+          </svg>
+          <img
+            className="presentation__buttonSection__button__play"
+            src="src/assets/presentation-play.svg"
+            alt=""
+          />
+        </button>
+      </section>
+
+      <button className='presentation__button'>Explora el lugar</button>
+    </div>
   )
 }
 
