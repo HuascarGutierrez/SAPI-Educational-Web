131340f9f721040e749a34f35762473432ddee45
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2025-02-17 08:41:41 -0400

    fixing mistakes

--- src/components/molecules/Clase.jsx
+++ src/components/molecules/Clase.jsx
@@ -5,9 +5,11 @@ function Clase({clase}) {
   return (
     <div className='clase'>
         <img className='clase__img' src={clase.imageUrl} alt="" />
-        <h4 className='clase__h4'>{clase.name}</h4>
-        <p className='clase__p'>{clase.description}</p>
-        <button className='clase__button'>Toma ya</button>
+        <div className='clase__description'>
+          <h4 className='clase__h4'>{clase.name}</h4>
+          <p className='clase__p'>{clase.description}</p>
+          <button className='clase__button'>Toma ya</button>
+        </div>
     </div>
   )
 }
