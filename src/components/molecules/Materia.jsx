00bff80a306e579c7aef7d582805152e3d198ef0
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2025-02-16 20:45:03 -0400

    materias done

--- src/components/molecules/Materia.jsx
+++ src/components/molecules/Materia.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types'
 import './styles/Materia.css'
-
+import { useState } from 'react'
 function Materia({materia}) {
+  const [hover, setHover] = useState(false)
   return (
-    <div className='materia'>
+    <div onMouseEnter={() => setHover(true)}
+    onMouseLeave={() => setHover(false)} className='materia'>
         <img className='materia_image' src={materia.imageUrl} alt={materia.name} />
         <span className='materia_span'>{materia.name}</span>
         <p className='materia_p'>{materia.description}</p>
+        <button className='materia_button'>{hover?'Explora el curso':'More'}</button>
     </div>
   )
 }
